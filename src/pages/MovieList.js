411df085d869bc690a203067f2b8ddf9b778c159
Sultan <a_sultan_93@mail.ru>
2023-06-15 18:42:15 +0500

    asdf

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -4,7 +4,7 @@ import propTypes from 'prop-types';
 export const MovieList = ({movies}) => {
     const location = useLocation();
     return(
-            <>
+            <div>
                 {movies.map(movie => (
                     <li key={movie.id}>
                         <Link className="MovieLink" to={`/movies/${movie.id}`} state={{from:location}}>
@@ -12,7 +12,7 @@ export const MovieList = ({movies}) => {
                         </Link>
                     </li>
                 ))}
-            </>
+            </div>
     );
 };
 
