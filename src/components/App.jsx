8810d4079063810a9b7a32f5dab5262a18b9a526
Sultanbek <75874726+Sultanbek-hash@users.noreply.github.com>
2023-06-15 18:27:32 +0500

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,7 @@ export const App = () => {
   return (
     <>
     <Routes>
-      <Route path="/" element={<SharedLayout />}>
+      <Route path="/goit-react-hw-05-movies/" element={<SharedLayout />}>
         <Route index element={<Home />} />
         <Route path="/movies" element={<Movies />} />
 
